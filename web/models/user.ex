5c071ad9274f22711c1cf4a9c82d3ec29fe90420
Michael Johnston <lastobelus@mac.com>
2016-03-24 13:01:13 -0700

    add hashing of password to user model
    and users/new
    
    brings to Ch.5 Authenticating Users > Creating Users

--- web/models/user.ex
+++ web/models/user.ex
@@ -14,4 +14,28 @@ defmodule Rumbl. User do
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
   end
+
+  # diffferent validation strategies, and/or combinations
+  # of optional/required fields are accomplished simply
+  # by defining different changeset functions
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    # persistence is not strongly coupled to our change policies!
+    # so we can validate virtual fields the same as we validate
+    # database attributes
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
+  end
 end
