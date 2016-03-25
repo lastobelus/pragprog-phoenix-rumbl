b91aab0e55a607e2c4215c190231d998f039eaa8
Michael Johnston <lastobelus@mac.com>
2016-03-24 20:24:10 -0700

    authenticate plug with login
    brings up to Ch. 5 Authenticating Users > Writing an Authentication Plug

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,6 +2,8 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
+
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
@@ -22,10 +24,23 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
     {:ok, user} ->
       conn
+      |> Rumbl.Auth.login(user)
       |> put_flash(:info, "#{user.name} created.")
       |> redirect(to: user_path(conn, :index))
     {:error, changeset} ->
       render(conn, "new.html", changeset: changeset)
     end
   end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page.")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
+
 end
