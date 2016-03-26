e04bf37526c74adbe986c470503919717c852138
Michael Johnston <lastobelus@mac.com>
2016-03-25 21:28:31 -0700

    scope videos to user
    override VideoController#action to provide
    current_user as third param to every action
    use Repo#build_assoc, and Repo#assoc to scope
    videos to current_user everywhere in VideoController
    
    Brings to end of Ch. 6 Generators and Relationships

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,10 +1,12 @@
-defmodule Rumbl. User do
+defmodule Rumbl.User do
   use Rumbl.Web, :model
   schema "users" do
     field :name, :string
     field :username, :string
     field :password, :string, virtual: true
     field :password_hash, :string
+
+    has_many :videos, Rumbl.Video
 
     timestamps
   end
